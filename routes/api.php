5063f49e22948f31d7b6fadc811a9fb1c9827bae
Arianfh <ariansyahfadilah@gmail.com>
2023-08-16 11:10:49 +0700

    membuat route api kelas dan siswa

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\KelasController;
+use App\Http\Controllers\SiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('kelas', [KelasController::class, 'index']);
+Route::post('kelas/tambah', [KelasController::class, 'store']);
+Route::get('kelas/tampil/{id}', [KelasController::class, 'show']);
+Route::get('kelas/{id}/edit', [KelasController::class, 'edit']);
+Route::put('kelas/{id}/edit', [KelasController::class, 'update']);
+
+Route::get('siswa', [SiswaController::class, 'index']);
+Route::post('siswa/tambah', [SiswaController::class, 'store']);
