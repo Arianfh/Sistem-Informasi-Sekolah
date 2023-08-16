048415351c826e29217e4707224a7d230c0c3355
Arianfh <ariansyahfadilah@gmail.com>
2023-08-16 11:09:29 +0700

    tambah migration siswa dan kelas

---
+++ database/migrations/2023_08_15_113253_create_kelas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $collection) {
+            $collection->id();
+            $collection->string('kelas');
+            $collection->string('wali_kelas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
---
+++ database/migrations/2023_08_15_113513_create_siswa_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $collection) {
+            $collection->id();
+            $collection->string('nama');
+            $collection->foreignId('kelas_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
